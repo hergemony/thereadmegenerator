e371c236d7dd4999f87044a50eb0c6f63ea83c2b
Chi Doan <chi.doan86@gmail.com>
2022-04-18 14:45:42 +0800

    update

--- app.js
+++ app.js
@@ -13,37 +13,92 @@
 
 //this app is used to generate a readme file for our user
 //use inquirer to ask user questions
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const readme = require('./node_modules/readme');
+const fs = require('fs');
+
+
+console.log(readme);
 
 inquirer.prompt([
     {
         type: "input",
         message: "What is the title of your project?",
-        name: "Title",
+        name: "title",
     },
     {
         type: "input",
         message: "What is the description of your project?",
-        name: "Description",
+        name: "description",
 
     },
     {
         type: "input",
         message: "Create your Table of Contents",
-        name: "Table of Contents",
+        name: "table of contents",
     },
 {
     type: "input",
     message: "Installation Instructions",
-    name: "Installation Instructions",
+    name: "installation instructions",
 },
 {
     type: "input",
     message: "Usage Information",
-    name: "Usage Information",
+    name: "usage information",
 },
+{
+        type: "list",
+        message: "What license is the application covered under?",
+        choices: [
+            "MIT",
+            "Apache-2.0",
+            "GPL 3.0",
+            "BSD 2 Clause",
+            "BSD 3 Clause",
+            "BSD 4 Clause",
+        ],
+        name: "license",
+    },
+    {
+
+        type: "input",
+        message: "What tests have been performed?",
+        name: "tests",
+    },
+    {
+        type:"input",
+        message: "Insert Screenshots of the application",
+        name: "screenshots",
+    },
+    {
+
+        type: "input",
+        message: "Insert Demo video of the application",
+        name: "video Demo",
+    },
+    {
+        type: "input",
+        message: "Acknowledgements",
+        name: "Acknowledgements",
+    },
+    {
+        type: "input",
+        message: "Insert Company Footer/Signature/Copyright",
+        name: "Copyright",
+    },
+        
+
 
 ]).then(function(answers){
 
     console.log(answers);
+
+    const readmeContent = readme.generateReadme(answers);
+    
+    //save the readme content to a README.md file
+    
+    fs.writeFileSync(__dirname + '/readme.md', readmeContent, 'utf-8');
+
 });
+
--- readme.js
+++ readme.js
@@ -1,12 +1,16 @@
-function generateReadMe(answers){
+function generateBadge(license){
+    return `https://img.shields.io/badge/License-${encodeURIComponent(license)}-blue`;
+}
 
-    //main logic to generate readme
+function generateReadme(answers){
+    // main logic to generate readme
+return `
 
-    return `
+# ${answers.title}
 
-# ${answers.Title}
+![License](${generateBadgeUrl(answers.license)})
 
-${answers.Description}
+${answers.description}
 
 
 ## Description
@@ -19,32 +23,43 @@ Use the following questions as a guide:
 - What problem does it solve?
 - What did you learn?
 
+${answers.tableofcontents}
+
 ## Table of Contents (optional)
 
 If your README is long, add a table of contents to make it easy for users to find what they need
 
+${answers.installationinstructions}
 ## Installation
 What are the steps required to install your project? 
 Provide a step-by-step description of how to get the development environment up and running.
 
+
+${answers.usage}
 ## Usage
 
+${answers.description}
 ## License
 The last section of a high-quality README file is the license. This lets other developers know what they can and 
 cannot do with your project.
 
+${answers.tests}
 ## Tests
 
+${answers.screenshots}
 ## Screenshots
 
+${answers.videodemo}
 ## Video Demo
 
-## Github Features
-
+${answers.acknoledgements}
 ## Acknowledgements 
 List your collaborators, if any, with links to their GitHub profiles.
 If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 If you used tutorials, include links to those here as well.
 
+
+${answers.copyright}
 ## Copyright 
 
+}
